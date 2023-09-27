b0113c6b971d3f992f86770d9d9466fd00c649e9
Jordan Webb <jordan@webb.haus>
2023-09-27 04:17:51 +0000

    Wrap in qodem when necessary

--- launch.sh
+++ launch.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ "$TERM" != "ansi" ] && [ "$DOORMAN_RAW" != "1" ] ; then
+  if [ -z "${IN_QODEM:-}" ] ; then
+    IN_QODEM=1 exec qodem \
+      -x \
+      --read-only \
+      --codepage CP437 \
+      --emulation ansi \
+      --status-line off \
+      "$0"
+  fi
+fi
+
 echo $$ > /mnt/doorman/client.pid
 echo "Waiting for door to start..."
 
